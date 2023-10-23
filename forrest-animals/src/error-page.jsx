6a15be4e89dacd76439caf62e1b6b9278e208045
Judith Bosmans <judith.bosmans@student.ehb.be>
2023-10-23 11:18:35 +0200

    Error page made

---
+++ forrest-animals/src/error-page.jsx
@@ -0,0 +1,25 @@
+import { useRouteError } from "react-router-dom";
+
+import styles from "./styles/ErrorPage.css";
+import cat from "./pics/cat.gif";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <div id="container2">
+        <img src={cat} id="cat"/>;
+      </div>
+      <div id="container1">
+        <h1>Woops!</h1>
+        <p id="text">An unexpected error has occurred.</p>
+        <p id="text2">My bad.</p>
+        <p id="errorMessage">
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ forrest-animals/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./index.css";
+
+import ErrorPage from "./error-page";
+import App from "./components/App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      // {
+      //   path: "/CherryPage",
+      //   element: <Cherry />,
+      //   errorElement: <ErrorPage />,
+      // },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
